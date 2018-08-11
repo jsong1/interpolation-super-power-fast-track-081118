a1bdd362279dc6d46651106b4d94fa50948d1eb2
jginasong@gmail.com Song <jginasong@gmail.com>
2018-08-11 16:40:35 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(color_array)
-colors = ['red', 'orange','yellow','green','blue','indigo','violet']
+#display_rainbow = ['red', 'orange','yellow','green','blue','indigo','violet']
 puts "colors"
 end
